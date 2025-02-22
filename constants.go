646b4971da229f571332547333dab00c78dc9c0f
Nya Candy <dev@candinya.com>
2025-02-22 16:19:10 +0800

    chore: remove unused constants

--- constants.go
+++ constants.go
@@ -3,7 +3,6 @@ package main
 import "time"
 
 const (
-	//DefaultPort       = 25220 // Project start at 2025-02-20
 	DefaultRendezvousString  = "clipboard"
 	DefaultProtocolID        = "/peerclip/v1"
 	MessageDelim             = '\n'
